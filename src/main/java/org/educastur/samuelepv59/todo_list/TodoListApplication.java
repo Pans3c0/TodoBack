d0ca0b6b56c0ace1ea6814ae44df5035257ba699
pans3c0 <samupachebq@gmail.com>
2026-05-23 02:32:27 +0200

    .

--- src/main/java/org/educastur/samuelepv59/todo_list/TodoListApplication.java
+++ src/main/java/org/educastur/samuelepv59/todo_list/TodoListApplication.java
@@ -1,13 +1,28 @@
 package org.educastur.samuelepv59.todo_list;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.env.Environment;
+
 @SpringBootApplication
-@ComponentScan(basePackages = "org.educastur.samuelepv59.todo_list") // <--- FUERZA EL ESCANEO
+//@ComponentScan(basePackages = "org.educastur.samuelepv59.todo_list") // <--- FUERZA EL ESCANEO
 public class TodoListApplication {
+
+	@Autowired
+	private Environment env;
+
+	@PostConstruct
+	public void init() {
+		System.out.println(">>> ARCHIVO CARGADO: " + env.getProperty("spring.application.name"));
+		System.out.println(">>> URL DATASOURCE: " + env.getProperty("spring.datasource.url"));
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodoListApplication.class, args);
 	}
 }
